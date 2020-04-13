4590722294b308258cf1976e58b66044b34b67dd
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2020-04-13 10:24:33 -0500

    Add files via upload

--- AutoHome.ino
+++ AutoHome.ino
@@ -2,9 +2,21 @@
    Elaborado por: Acosta Rodríguez Eder Alberto. | Nombre del Proyecto: AutoHome
    Correo de contacto: ederacosta473@gmail.com*/
 #include<Servo.h>
+#include <Keypad.h>
 Servo Servito;
-int val = 500;
+int val = 500,sensor = 2,nivel = 0;
 const int receptor = 7, transmisor = 8, garage_ocupado = 2, garage_disponible = 1;
+const byte filas = 4;
+const byte columnas = 4;
+const byte pin_filas[filas] = { 11, 10, 9, 8 };
+const byte pin_columna[columnas] = { 7, 6, 5, 4 };
+char coordenadas[filas][columnas] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '#', '0', '*', 'D' }
+};
+Keypad keypad = Keypad(makeKeymap(coordenadas), pin_filas, pin_columna, filas, columnas);
 void setup()
 {
   //Inicializamos la tarjeta arduino
@@ -13,6 +25,9 @@ void setup()
   Servito.attach(9);
   pinMode(receptor, INPUT);
   pinMode(transmisor, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(4, INPUT);
 }
 void loop()
 {
@@ -36,7 +51,27 @@ void loop()
     delay(distancia*10);
   }
   //Sección del servo para abrir la puerta principal
+  char tecla = keypad.getKey();
+
+  if (tecla) {
+    Serial.println(tecla);
+  }
+  switch (tecla){
+    case'1':
+      digitalWrite(3, HIGH);
+    break;
+    case'2':
+      digitalWrite(3, LOW);
+    break;    
+    case'3':
+      digitalWrite(12, HIGH);
+    break;    
+    case'4':
+      digitalWrite(12, LOW);
+    break;
+  }
   Servito.write(45);
   delay(val);
-  //Sección 
+  //Sección del sensor de luz
+  
 }
