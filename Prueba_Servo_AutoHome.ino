12cf2232afb4203c076225f8a7115afdbfd2cc66
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2020-05-06 10:48:13 -0500

    Add files via upload

---
+++ Prueba_Servo_AutoHome.ino
@@ -0,0 +1,14 @@
+#include<Servo.h>
+Servo servo1;
+int PinServo = 9, PulsoMin = 900, PulsoMax = 2100;
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(PinServo, PulsoMin, PulsoMax);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  servo1.write(0);
+  delay(3000);
+  servo1.write(180);
+  delay(3000);
+}
