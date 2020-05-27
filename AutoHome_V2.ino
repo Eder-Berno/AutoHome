5c217692d110626488aa2c89f9d9e2eb64b67c18
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2020-05-26 19:38:23 -0500

    Add files via upload

---
+++ AutoHome_V2.ino
@@ -0,0 +1,41 @@
+/*Universidad Nacional Autónoma de México. | Facultad de Ingeniería.
+   Elaborado por: Acosta Rodríguez Eder Alberto. | Nombre del Proyecto: AutoHome
+   Correo de contacto: ederacosta473@gmail.com*/
+//Declaraciones de LDR.
+int ledPin = 11; // Led en Pin 11
+int ldrPin = A4; // LDR en el pin analogico 4
+int ldrValue = 0;
+//Declaraciones de Teclado Matricial.
+#include <Keypad.h>
+const byte FILAS = 4;
+const byte COLUMNAS = 4;
+char keys[FILAS][COLUMNAS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte pinesFilas[FILAS] = {9,8,7,6};
+byte pinesColumnas[COLUMNAS] = {5,4,3,2};
+Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
+char Tecla, Clave[7], Clave_Correcta[7] = "231019";
+byte Indice = 0;
+------------------------------------------------------------------------------------------------------------
+void setup() 
+{
+  Serial.begin(9600);
+  // Proceso LDR.
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,LOW);
+}
+------------------------------------------------------------------------------------------------------------
+void loop() 
+{
+  // Proceso LDR.
+  ldrValue = analogRead(ldrPin); //entre 0 y 1023
+  Serial.println(ldrValue);
+  ldrValue = (ldrValue*255)/1023;
+  analogWrite(ledPin, ldrValue);
+  delay(500);
+  // Proceso Teclado Matricial.
+}
---
+++ Teclado_Matricial_Prueba_AutoHome.ino
@@ -0,0 +1,45 @@
+#include <Keypad.h>
+int ledPin = 11;
+int Buzzer = 12;
+const byte FILAS = 4;
+const byte COLUMNAS = 4;
+char keys[FILAS][COLUMNAS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte pinesFilas[FILAS] = {9,8,7,6};
+byte pinesColumnas[COLUMNAS] = {5,4,3,2};
+Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
+char Tecla, Clave[7], Clave_Correcta[7] = "231019";
+byte Indice = 0;
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(Buzzer, OUTPUT);
+}
+
+void loop() 
+{
+    digitalWrite(Buzzer, LOW);
+    Tecla = teclado.getKey();
+    if (Tecla){
+      Clave[Indice] = Tecla;
+      Indice++;
+      Serial.print(Tecla);
+    }
+    if (Indice == 6){
+      if(!strcmp(Clave, Clave_Correcta)){
+      analogWrite(ledPin, 0);
+      digitalWrite(Buzzer, LOW);
+      delay(20);}
+      else{
+      tone(Buzzer, 200);
+      analogWrite(ledPin, 255);
+      delay(100);
+      noTone(Buzzer);
+      delay(100);
+      Indice = 0;}
+    }
+}
